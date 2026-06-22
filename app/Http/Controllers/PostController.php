446e046929cc7ec0dc7f1eb1bbaee4bc3000fc08
outamghartAchraf <outamghart2000@gmail.com>
2026-06-22 17:11:21 +0100

    add function index for show list posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
-    //
+    public function index() 
+    {
+        $posts = Post::with('user')->latest()->get();
+        return view('posts.index', compact('posts'));
+    }
 }
