856e0e44f21ea4bfec07aae5711a453cafdc4c01
outamghartAchraf <outamghart2000@gmail.com>
2026-06-29 11:54:52 +0100

    add function showRegisterForm and create page register.blade.php

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,9 @@
 
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\UserController;
 
 Route::get('/', [PostController::class, 'index'])->name('posts.index');
+
+
+Route::get('/register', [UserController::class, 'showRegistrationForm'])->name('register');
