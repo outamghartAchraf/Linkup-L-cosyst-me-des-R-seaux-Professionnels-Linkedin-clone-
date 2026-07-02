459a07b6a55ed80b0da976429b6f5a0f22e3f8a2
outamghartAchraf <outamghart2000@gmail.com>
2026-07-02 14:49:13 +0100

    ceate function toggle for like post

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -2,9 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class LikeController extends Controller
 {
-    //
+    public function toggle(Post $post)
+    {
+        $user = auth()->user();
+
+        $like = $post->likes()
+            ->where('user_id', $user->id)
+            ->first();
+
+        if ($like) {
+
+            $like->delete();
+        } else {
+ 
+            $post->likes()->create([
+                'user_id' => $user->id,
+            ]);
+        }
+
+        return back();
+    }
 }
