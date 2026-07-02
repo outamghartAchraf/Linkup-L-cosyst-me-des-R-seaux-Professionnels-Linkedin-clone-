91432d912dfc6f2f541788fe45f1c7fed3087e6f
outamghartAchraf <outamghart2000@gmail.com>
2026-07-02 15:31:19 +0100

    add relation ship between models

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -25,4 +25,9 @@ class Post extends Model
     {
         return $this->hasMany(Like::class);
     }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
