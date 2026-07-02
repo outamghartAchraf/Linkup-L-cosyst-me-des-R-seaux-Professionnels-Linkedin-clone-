2603fe03dbe82f1506c887f0924031a46c7d3430
outamghartAchraf <outamghart2000@gmail.com>
2026-07-02 15:29:33 +0100

    create table comments and add cuolm

--- database/migrations/2026_07_02_142720_create_comments_table.php
+++ database/migrations/2026_07_02_142720_create_comments_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')
+                ->constrained()
+                ->cascadeOnDelete();
+
+            $table->foreignId('post_id')
+                ->constrained()
+                ->cascadeOnDelete();
+
+            $table->text('content');
             $table->timestamps();
         });
     }
