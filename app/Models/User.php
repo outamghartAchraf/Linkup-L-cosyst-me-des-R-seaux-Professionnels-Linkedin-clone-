66216ba6421a81d062c7fdc8d320bac3f3de0316
outamghartAchraf <outamghart2000@gmail.com>
2026-07-06 14:38:44 +0100

    add function of relationShip

--- app/Models/User.php
+++ app/Models/User.php
@@ -64,4 +64,20 @@ class User extends Authenticatable
     {
         return $this->hasMany(Comment::class);
     }
+
+ 
+    public function following()
+    {
+        return $this->belongsToMany(
+
+            User::class,
+
+            'follows',
+
+            'follower_id',
+
+            'following_id'
+
+        );
+    }
 }
